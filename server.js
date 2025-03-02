9a9a0ec9ab39c38cc40facb6d757d8f81b90e663
vilgaxx <codervilgax@gmail.com>
2025-03-02 16:27:22 +0530

    server update

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-import e from 'express';
-import fs from 'fs';
-import path from 'path';
-import {fileURLToPath} from "url";
-
+import e from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = e();
-const port = 300;
-app.use(e.static('public'));
+const port = 3000;
+app.use(e.static("public"));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get('/songs', (req, res) => {
-    const songDir = path.join(__dirname, "public/songs/Mix/");
-    fs.readdir(songDir, (err, files) => {
-        if(err) {
-            console.error("error");
-            res.status(500).json({error: "failed to load songs"})
-        }
-        else {
-            const songs = files.filter(file => file.endsWith('.mp3'));
-            res.json(songs);
-        }
-    })
-})
+function getSongsFromCategory(category) {
+  const categoryPath = path.join(__dirname, `public/songs/${category}`);
+
+  if (!fs.existsSync(categoryPath)) return [];
+
+  return fs
+    .readdirSync(categoryPath)
+    .filter((file) => file.endsWith(".mp3"))
+    .map((file) => ({
+      name: file,
+      url: `/songs/${encodeURIComponent(category)}/${encodeURIComponent(file)}`,
+    }));
+}
+app.get("/songs", (req, res) => {
+  const categories = ["English", "Mix", "NoMusic"];
+  const songsData = {};
+
+  categories.forEach((category) => {
+    songsData[category] = getSongsFromCategory(category);
+  });
+
+  res.json(songsData);
+});
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-})
+  console.log(`http://localhost:${port}`);
+});
